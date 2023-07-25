1edfc3010bbd80e57d74fe757e57be6cb1961103
LINGGUANG <17671304431@163.com>
2023-07-25 10:08:29 +0800

    Initial commit

---
+++ plugins/bak/setup.lua
@@ -0,0 +1,17 @@
+return {
+    {
+        -- 添加插件
+
+        'Exafunction/codeium.vim',
+
+        -- "christoomey/vim-tmux-navigator", -- 用ctl-hjkl来定位窗口
+       
+        'junegunn/fzf',
+        'junegunn/fzf.vim',
+      
+        'francoiscabrol/ranger.vim',
+      
+      
+
+    },
+}
